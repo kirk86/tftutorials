a42512886aeee881ada9f01c329313376c7d3af1
kirk86 <kirk86@walla.com>
2018-06-25 03:54:03 +0000

    decorators for cleaner tf graphs

--- tf_scope_decorator.py
+++ tf_scope_decorator.py
@@ -10,8 +10,9 @@ def doublewrap(function):
     A decorator decorator, allowing to use the decorator to be used without
     parentheses if no arguments are provided. All arguments must be optional.
     """
-    @functools.wraps(function)
+    @functools.wraps(function)  # wrapper function preserving information being passed
     def decorator(*args, **kwargs):
+        # Optional arg decorator. Decorator wiht optional arguments
         if len(args) == 1 and len(kwargs) == 0 and callable(args[0]):
             return function(args[0])
         else:
